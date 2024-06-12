4c27b27ae10635e0482fc4efc9c96db0b296bffe
Claudia <123874337+Claudiapf7@users.noreply.github.com>
2024-06-12 19:04:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 
 <?php 
-    $name = "Claudia";
-    $isDev = true;
-    $age = 29;
-    $output = "Hola $name";
-    $output .= " con una edad de $age";
     const TOKEN = "2ASQglKA7xAEsydHgZFCbsFaQiTvXXQTIcBQw3Q862RUaWNCm16kdZvdfly3";
     $bestLanguages =["PHP", "Javascript", "Java", "Python"];
 ?>
@@ -16,15 +11,13 @@
     $result = curl_exec($ch);
     $data = json_decode($result, true);
     curl_close($ch);
-
-    var_dump($data);
 ?>
-
-<h1>
-    <?= $output?>
-</h1>
-
-<style>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Days until next Marvel Movie</title>
+    </head>
+    <style>
     :root{
         color-scheme: light dark;
     }
@@ -33,9 +26,36 @@
         display: grid;
         place-content: center;
     }
-</style>
-<ul>
-    <?php foreach ($bestLanguages as $key => $l): ?>
-        <li><?= $key . " => ".$l ?></li>
-    <?php endforeach; ?>
-</ul>
+
+    img{
+        margin: 0 auto;
+        border-radius: 20px;
+    }
+
+    section{
+        display: flex;
+        justify-content: center;
+    }
+
+    hgroup{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+    </style>
+    <main>
+        <h1>Próximos estrenos de Marvel</h1>
+        <section>
+            <img src="<?= $data["poster_url"]; ?>" width="300px" alt="Poster de <?= $data["title"]; ?>"
+            />
+        </section>
+
+        <hgroup>
+            <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]?> días</h3>
+            <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
+            <p>La siguiente es: <?= $data["following_production"]["title"]; ?></p>
+        </hgroup>
+    </main>
+
+</html>
